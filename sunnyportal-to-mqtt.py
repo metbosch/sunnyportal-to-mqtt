c70ce08e37191cec57f18bb4c40609fbefa6f2da
User <user@server>
2022-04-13 10:01:43 +0000

    Do not keep sunnyportal client between retrievements

--- sunnyportal-to-mqtt.py
+++ sunnyportal-to-mqtt.py
@@ -15,17 +15,14 @@ def main():
     client_mqtt = mqtt_client.Client()
     client_mqtt.username_pw_set(config['mqtt']['username'], config['mqtt']['password'])
     client_mqtt.connect(config['mqtt']['host'], config['mqtt']['port'])
-    client_sp = sunnyportal.client.Client(config['sunnyportal']['email'], config['sunnyportal']['password'])
     while True:
-      try:
-        for plant in client_sp.get_plants():
-          data = plant.last_data_exact(date.today() + timedelta(days=1))
-          print(f"{datetime.now()}: wh={data.hour.absolute} plant={plant.name}")
-          data_mqtt = '{"plant": "' + plant.name + '", "prod_wh": ' + str(data.hour.absolute) + '}'
-          client_mqtt.publish(config['mqtt']['topic'], data_mqtt)
-      except Exception as e:
-        print("An exception happened: " + str(e))
-        exit(1)
+      print(f"{datetime.now()}: Starting sunnyportal information retrievement")
+      client_sp = sunnyportal.client.Client(config['sunnyportal']['email'], config['sunnyportal']['password'])
+      for plant in client_sp.get_plants():
+        data = plant.last_data_exact(date.today() + timedelta(days=1))
+        print(f"{datetime.now()}: wh={data.hour.absolute} plant={plant.name}")
+        data_mqtt = '{"plant": "' + plant.name + '", "prod_wh": ' + str(data.hour.absolute) + '}'
+        client_mqtt.publish(config['mqtt']['topic'], data_mqtt)
       seconds_delay = random.randrange(config['period']['min'], config['period']['max'])
       time.sleep(seconds_delay)
 
